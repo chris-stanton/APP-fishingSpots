3a7fcca2be72063e9cc337bdc465b8e6f246c968
Chris Stanton <cstanton0760@yahoo.com>
2018-01-02 20:45:42 -0600

    starting sql db buildout

--- database.sql
+++ database.sql
@@ -3,11 +3,112 @@
 	-- create database titled (hardwareUI)
 
 -- step 2
+	-- creates users table for authentication
 	CREATE TABLE users (
 		id SERIAL PRIMARY KEY,
 		username VARCHAR(100) NOT NULL unique,
 		password VARCHAR(100) NOT NULL,
 		email VARCHAR(100) NOT NULL
+	);
+	-- creates species specifics tables
+	CREATE TABLE blueGill (
+		id SERIAL PRIMARY KEY,
+		detail_spring VARCHAR(2000),
+		detail_summer VARCHAR(2000),
+		detail_fall VARCHAR(2000),
+		detail_earlyIce VARCHAR(2000),
+		detail_midWinter VARCHAR(2000),
+		detail_lateIce VARCHAR(2000),
+		summary VARCHAR(5000),
+		picURL VARCHAR(500),
+		active boolean DEFAULT true
+	);
+	CREATE TABLE blackCrappie (
+		id SERIAL PRIMARY KEY,
+		detail_spring VARCHAR(2000),
+		detail_summer VARCHAR(2000),
+		detail_fall VARCHAR(2000),
+		detail_earlyIce VARCHAR(2000),
+		detail_midWinter VARCHAR(2000),
+		detail_lateIce VARCHAR(2000),
+		summary VARCHAR(5000),
+		picURL VARCHAR(500),
+		active boolean DEFAULT true
+	);
+	CREATE TABLE whiteCrappie (
+		id SERIAL PRIMARY KEY,
+		detail_spring VARCHAR(2000),
+		detail_summer VARCHAR(2000),
+		detail_fall VARCHAR(2000),
+		detail_earlyIce VARCHAR(2000),
+		detail_midWinter VARCHAR(2000),
+		detail_lateIce VARCHAR(2000),
+		summary VARCHAR(5000),
+		picURL VARCHAR(500),
+		active boolean DEFAULT true
+	);
+	CREATE TABLE perch (
+		id SERIAL PRIMARY KEY,
+		detail_spring VARCHAR(2000),
+		detail_summer VARCHAR(2000),
+		detail_fall VARCHAR(2000),
+		detail_earlyIce VARCHAR(2000),
+		detail_midWinter VARCHAR(2000),
+		detail_lateIce VARCHAR(2000),
+		summary VARCHAR(5000),
+		picURL VARCHAR(500),
+		active boolean DEFAULT true
+	);
+	CREATE TABLE walleye (
+		id SERIAL PRIMARY KEY,
+		detail_spring VARCHAR(2000),
+		detail_summer VARCHAR(2000),
+		detail_fall VARCHAR(2000),
+		detail_earlyIce VARCHAR(2000),
+		detail_midWinter VARCHAR(2000),
+		detail_lateIce VARCHAR(2000),
+		summary VARCHAR(5000),
+		picURL VARCHAR(500),
+		active boolean DEFAULT true
+	);
+
+	CREATE TABLE northernPike (
+		id SERIAL PRIMARY KEY,
+		detail_spring VARCHAR(2000),
+		detail_summer VARCHAR(2000),
+		detail_fall VARCHAR(2000),
+		detail_earlyIce VARCHAR(2000),
+		detail_midWinter VARCHAR(2000),
+		detail_lateIce VARCHAR(2000),
+		summary VARCHAR(5000),
+		picURL VARCHAR(500),
+		active boolean DEFAULT true
+	);
+
+	CREATE TABLE musky (
+		id SERIAL PRIMARY KEY,
+		detail_spring VARCHAR(2000),
+		detail_summer VARCHAR(2000),
+		detail_fall VARCHAR(2000),
+		detail_earlyIce VARCHAR(2000),
+		detail_midWinter VARCHAR(2000),
+		detail_lateIce VARCHAR(2000),
+		summary VARCHAR(5000),
+		picURL VARCHAR(500),
+		active boolean DEFAULT true
+	);
+
+	CREATE TABLE burbot (
+		id SERIAL PRIMARY KEY,
+		detail_spring VARCHAR(2000),
+		detail_summer VARCHAR(2000),
+		detail_fall VARCHAR(2000),
+		detail_earlyIce VARCHAR(2000),
+		detail_midWinter VARCHAR(2000),
+		detail_lateIce VARCHAR(2000),
+		summary VARCHAR(5000),
+		picURL VARCHAR(500),
+		active boolean DEFAULT true
 	);
 
 -- step 3:
