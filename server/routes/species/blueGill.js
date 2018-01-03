df8aacdce654b1ca21722a3336caa3cd52548a74
Chris Stanton <cstanton0760@yahoo.com>
2018-01-02 22:04:56 -0600

    ending server side buildout for the night

--- server/routes/species/blueGill.js
+++ server/routes/species/blueGill.js
@@ -3,20 +3,9 @@ const router = require('express').Router();
 const pg = require('pg');
 const pool = require('../../modules/database-config');
 
-router.get('/', function(req, res) {
-  // pool.connect()
-  //   .then(function (client) {
-  //     client.query("SELECT id, name FROM users")
-  //       .then(function (result) {
-  //         client.release();
-  //         res.send(result.rows);
-  //       })
-  //       .catch(function (err) {
-  //         console.log('error on SELECT', err);
-  //         res.sendStatus(500);
-  //       });
-  console.log('hit');
-    });//end of .then
+// summer 8-12' or 30-40'
+
+
 
 
 
