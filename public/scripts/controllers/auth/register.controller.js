03bad6d9475b3f23387ed6064c14339b78decb22
Chris Stanton <cstanton0760@yahoo.com>
2018-01-05 23:19:04 -0600

    added error messageing

--- public/scripts/controllers/auth/register.controller.js
+++ public/scripts/controllers/auth/register.controller.js
@@ -21,7 +21,6 @@ myApp.controller('RegisterController', ['$location', '$http', 'alertify', functi
       console.log('sending user credentials to server...', self.user);
       $http.post('/register', self.user).then(function(response) {
         console.log('registering user success');
-        // alertify.success("User registration completed");
         alertify.success('<div class="alertMessage">' +
                           '<img src="../../assets/images/icons/addUser.ico"> User registration completed' +
                         '</div>')
