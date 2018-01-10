f03b7c322bbd7c199f3dae0a936d84899618048a
Chris Stanton <cstanton0760@yahoo.com>
2018-01-09 22:11:06 -0600

    adedd to kuser_sql.js file

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -9,6 +9,7 @@ router.get('/getWeather', function (req, res) {
   let lat = req.headers.lat;
   let long = req.headers.long;
 
+  // create gobal var within heroku om next deploy
   request('https://api.darksky.net/forecast/' + process.env.DARK_SKY_KEY + '/' + lat + ',' + long, function (error, response, body) {
     res.send(body);
   });
