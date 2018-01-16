ecb19fe69c08d90c8fc6ab9b38a149d1a5cfa5be
Chris Stanton <cstanton0760@yahoo.com>
2018-01-15 19:33:19 -0600

    deploying

--- public/scripts/controllers/main.controller.js
+++ public/scripts/controllers/main.controller.js
@@ -44,7 +44,7 @@ myApp.controller('MainController',['InitFactory', 'SpeciesFactory', 'UserService
   // displays and settings for navionics map
   let webapi = new JNC.Views.BoatingNavionicsMap({
     tagId: '.test_map_div',
-    center: [  parseInt(coords.lat), parseInt(coords.long) ],
+    center: [ lat, long ],
     depthUnit: JNC.DEPTH_UNIT.FEET,
     distanceUnit: JNC.DISTANCE_UNIT.MILES,
     safetyDepth: JNC.SAFETY_DEPTH_LEVEL.FEET_20,
